3f3a0ba588730181280d3c75a9402d8735b3242a
Salman Farshee <salman@aveniripltd.com>
2023-05-24 18:57:01 +0600

    templates

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -3,7 +3,7 @@ export default function DashboardLayout({ children }) {
     <div className="w-full min-h-screen bg-white text-black">
       <div className="w-full h-auto mx-auto py-4 bg-orange-900">Layout Header</div>
       <div className="w-full h-[calc(100vh_-_56px)] flex">
-        <div className="w-[300px] bg-fuchsia-900">Layout Sidebar</div>
+        <div className="w-72 bg-fuchsia-900">Layout Sidebar</div>
         {children}
       </div>
     </div>
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,6 +1,6 @@
 export default function DashboardHome() {
   return (
-    <div className="w-full h-auto">
+    <div className="w-full h-auto bg-orange-600">
       <h1>Dashboard Home Page</h1>
     </div>
   );
